cee375a1bb51318d8788ba225974750ffe4f4c4e
zhaoweinan.vernon <zhaoweinan.vernon@byte.dance.com>
2018-11-13 23:15:44 +0800

    修改样式分类

--- src/components/widgets/controls/ximage.vue
+++ src/components/widgets/controls/ximage.vue
@@ -1,10 +1,6 @@
 <template>
   <div class="ximage-wrapper">
-    <el-form>
-      <el-form-item label="地址" label-width="50px">
-        <xupload v-model="props.url"/>
-      </el-form-item>
-    </el-form>
+    
   </div>
 </template>
 
@@ -12,14 +8,6 @@
   import xupload from "./util/xupload"
   import mixin from "../mixin-control"
   export default {
-    mixins: [mixin],
-    components: {
-      xupload
-    },
-    data() {
-      return {
-        props: {}
-      }
-    }
+    mixins: [mixin]
   }
 </script>
